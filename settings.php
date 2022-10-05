7f7712e0af6c607dd31c2d2b22be8b2b45101713
Annika Lambert <Annika.Lambert@ruhr-uni-bochum.de>
2022-10-05 09:32:21 +0200

    hide password

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
     $settings->add(new admin_setting_configtext('orcalti/orcalti_username', get_string('orcalti_username', 'orcalti'),
                        get_string('orcalti_username_details', 'orcalti'), '', PARAM_TEXT));
  
-    $settings->add(new admin_setting_configtext('orcalti/orcalti_password', get_string('orcalti_password', 'orcalti'),
+    $settings->add(new admin_setting_configpasswordunmask('orcalti/orcalti_password', get_string('orcalti_password', 'orcalti'),
                        get_string('orcalti_password_details', 'orcalti'), '', PARAM_TEXT));
                        
     $settings->add(new admin_setting_configtext('orcalti/orcalti_url', get_string('orcalti_url', 'orcalti'),
