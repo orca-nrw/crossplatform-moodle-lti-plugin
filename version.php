bd7481d27ad11bfc9a0fd99dca15e6e6ec39d2e6
Andrey Bachmaga <andrey.bachmaga@ampada.com>
2022-11-28 17:26:42 +0100

    Release Version: 20221128

--- version.php
+++ version.php
@@ -48,7 +48,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2022091500;    // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2022112800;    // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020060900;    // Requires this Moodle version.
 $plugin->component = 'mod_orcalti';     // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
